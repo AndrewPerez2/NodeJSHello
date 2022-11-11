4a5d97f0660c124bc6715511bf141ee6bc6499ae
AndrewPerez2 <aperez2@zagmail.gonzaga.edu>
2022-11-10 18:30:51 -0800

    participation pt 2

--- hello.js
+++ hello.js
@@ -1,2 +1,12 @@
+const http = require(http);
+const _ = require("underscore");
+
 var msg = "Hello, World at " + new Date() + "!";
 console.log(msg);
+
+http.createServer(respond).listen(8080);
+
+function respond(request, response){
+    response.write("<h1>Hello NodeJS " + _.random(1,100) +  "</h1>");
+    response.end();
+}
